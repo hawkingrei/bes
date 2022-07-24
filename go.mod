a81eb4d53e2d8cae793a2325c61dadcc8c048e75
Weizhen Wang <wangweizhen@pingcap.com>
2022-07-24 17:07:33 +0800

    : enable bes
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/hawkingrei/bes
+
+go 1.18
+
+require (
+	github.com/pingcap/log v1.1.0
+	go.uber.org/zap v1.19.0
+	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e
+	google.golang.org/genproto v0.0.0-20220722212130-b98a9ff5e252
+	google.golang.org/grpc v1.48.0
+	google.golang.org/protobuf v1.28.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	golang.org/x/text v0.3.5 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+)
---
+++ server/server.go
@@ -0,0 +1,134 @@
+package server
+
+import (
+	"context"
+	"io"
+	"net"
+
+	"github.com/hawkingrei/bes/model/parser"
+	pb "github.com/hawkingrei/bes/server/grpc"
+
+	"github.com/pingcap/log"
+	"go.uber.org/zap"
+	"golang.org/x/exp/slices"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type server struct {
+	pb.PublishBuildEventServer
+}
+
+func (*server) PublishBuildToolEventStream(stream pb.PublishBuildEvent_PublishBuildToolEventStreamServer) error {
+	// Semantically, the protocol requires we ack events in order.
+	acks := make([]int, 0)
+	var streamID *pb.StreamId
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if streamID == nil {
+			streamID = in.OrderedBuildEvent.StreamId
+		}
+		event := in.GetOrderedBuildEvent().Event
+		if event != nil {
+			event := event.GetEvent()
+			var details *anypb.Any
+			switch e := event.(type) {
+			case *pb.BuildEvent_InvocationAttemptStarted_:
+				details = e.InvocationAttemptStarted.Details
+			case *pb.BuildEvent_InvocationAttemptFinished_:
+				details = e.InvocationAttemptFinished.Details
+			case *pb.BuildEvent_BuildEnqueued_:
+				details = e.BuildEnqueued.Details
+			case *pb.BuildEvent_BuildFinished_:
+				details = e.BuildFinished.Details
+			case *pb.BuildEvent_ConsoleOutput_:
+			case *pb.BuildEvent_ComponentStreamFinished:
+			case *pb.BuildEvent_BazelEvent:
+			case *pb.BuildEvent_BuildExecutionEvent:
+			case *pb.BuildEvent_SourceFetchEvent:
+			default:
+				log.Error("unknown event type")
+			}
+			var data parser.BuildEvent
+			err := anypb.UnmarshalTo(details, &data, proto.UnmarshalOptions{})
+			if err != nil {
+				log.Error("failed to unmarshal event", zap.Error(err))
+				break
+			}
+			test := data.GetTestResult()
+			if test != nil &&
+				(test.GetStatus() == parser.TestStatus_FLAKY ||
+					test.GetStatus() == parser.TestStatus_FAILED ||
+					test.GetStatus() == parser.TestStatus_TIMEOUT) {
+				log.Info("test result", zap.String("result", test.GetStatusDetails()))
+			}
+		}
+		acks = append(acks, int(in.GetOrderedBuildEvent().SequenceNumber))
+	}
+	slices.Sort(acks)
+	for _, ack := range acks {
+		rsp := &pb.PublishBuildToolEventStreamResponse{
+			StreamId:       streamID,
+			SequenceNumber: int64(ack),
+		}
+		if err := stream.Send(rsp); err != nil {
+			log.Warn("Error sending ack stream for invocation", zap.String("InvocationId", streamID.InvocationId), zap.Error(err))
+			return err
+		}
+	}
+	return nil
+}
+
+func (*server) PublishLifecycleEvent(_ context.Context, req *pb.PublishLifecycleEventRequest) (*emptypb.Empty, error) {
+	event := req.BuildEvent.GetEvent().Event
+	var details *anypb.Any
+	switch e := event.(type) {
+	case *pb.BuildEvent_InvocationAttemptStarted_:
+		details = e.InvocationAttemptStarted.Details
+	case *pb.BuildEvent_InvocationAttemptFinished_:
+		details = e.InvocationAttemptFinished.Details
+	case *pb.BuildEvent_BuildEnqueued_:
+		details = e.BuildEnqueued.Details
+	case *pb.BuildEvent_BuildFinished_:
+		details = e.BuildFinished.Details
+	case *pb.BuildEvent_ConsoleOutput_:
+	case *pb.BuildEvent_ComponentStreamFinished:
+	case *pb.BuildEvent_BazelEvent:
+	case *pb.BuildEvent_BuildExecutionEvent:
+	case *pb.BuildEvent_SourceFetchEvent:
+	default:
+		log.Error("unknown event type")
+	}
+	var data parser.BuildEvent
+	err := anypb.UnmarshalTo(details, &data, proto.UnmarshalOptions{})
+	if err != nil {
+		log.Error("failed to unmarshal event", zap.Error(err))
+		return &emptypb.Empty{}, err
+	}
+	test := data.GetTestResult()
+	if test != nil &&
+		(test.GetStatus() == parser.TestStatus_FLAKY ||
+			test.GetStatus() == parser.TestStatus_FAILED ||
+			test.GetStatus() == parser.TestStatus_TIMEOUT) {
+		log.Info("test result", zap.String("result", test.GetStatusDetails()))
+	}
+	return &emptypb.Empty{}, nil
+}
+
+func Server() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterPublishBuildEventServer(s, &server{})
+	if err := s.Serve(listener); err != nil {
+		log.Fatal("failed to serve: %v", zap.Error(err))
+	}
+}
